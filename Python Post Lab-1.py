99ea3c5778f86c6ff766e0d74ace1d5be45b8ed9
Komara Mohan Gowda <komaramohangowda@gmail.com>
2025-09-09 11:14:09 +0530

    Add files via upload

---
+++ Python Post Lab-1.py
@@ -0,0 +1,11 @@
+//Write a program that displays “Welcome to Python” five times.
+st1="Welcome to Python "
+  print(st1*5)
+
+//Write a program that displays the following table
+print("a a^2 a^3")
+for a in range(1,5):
+    print(f"{a} {a*2} {a*3}")
+     
+//Write a program that displays the result of 
+print((9.5*4.5-2.5*3)/(45.5-3.5))
